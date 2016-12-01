aad077e026d7a12fe7af0e3477cd8496f36fcb23
Carla Corbal <carlacorbal@gmail.com>
2016-12-01 08:48:16 +0100

    Programa que pasa de grados celsius a fahrenheit

---
+++ CentigradosAFarenheit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CentigradosAFarenheit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double c, f;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduce la cantidad en ºC:");
+		c = sc.nextDouble();
+
+		f = 32 + (9 * c / 5);
+		
+		System.out.println("La cantidad equivale a " + f + "F");
+		
+		sc.close();
+	}
+
+}
